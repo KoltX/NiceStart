45312b7a7ded998120701d4bd865759ad010a5cf
Ernesto Ramiro <atomms@gmail.com>
2019-01-11 13:48:00 +0100

    Initial commit

---
+++ app/src/main/java/com/ramiro/ernesto/nicestart/LoginActivityCustom.java
@@ -0,0 +1,78 @@
+package com.ramiro.ernesto.nicestart;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+//import android.support.v7.app.ActionBarActivity;
+
+
+public class LoginActivityCustom extends Activity {
+
+   protected TextView mSignUpTextview;
+    protected TextInputLayout mUsername;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_custom);
+
+//        getSupportActionBar().hide();
+
+
+//        mUsername = (TextInputLayout)findViewById(R.id.passwordTIL);
+//        mUsername.requestFocus();
+
+
+        mSignUpTextview = (TextView)findViewById(R.id.signup_text);
+        mSignUpTextview.setOnClickListener(new View.OnClickListener(){
+            @Override
+                    public void onClick(View v){
+                Intent intent = new Intent(LoginActivityCustom.this, SignupActivity.class);
+                startActivity(intent);
+
+
+            }
+        });
+
+
+
+
+
+    }
+
+
+//    public void onClick (View textView){
+//        Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+//        startActivity(intent);
+//
+//    }
+
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_login, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
+}
