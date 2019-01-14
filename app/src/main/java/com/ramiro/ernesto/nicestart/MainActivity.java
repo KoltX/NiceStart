a20b9aeeda70cd7e52b25eab3187929844e2fc3d
Ernesto Ramiro <atomms@gmail.com>
2019-01-14 18:23:03 +0100

    added splash

--- app/src/main/java/com/ramiro/ernesto/nicestart/MainActivity.java
+++ app/src/main/java/com/ramiro/ernesto/nicestart/MainActivity.java
@@ -11,10 +11,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this, LoginActivityCustom.class);
-//        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-//        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        startActivity(intent);
+//        Intent intent = new Intent(this, LoginActivity.class);
+////        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+////        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+//        startActivity(intent);
 
     }
 }
