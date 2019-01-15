0c4043310b058f522885eee32c38a5cdea7db2a9
Ernesto Ramiro <atomms@gmail.com>
2019-01-15 17:24:07 +0100

    plus logo and fitting

--- app/src/main/java/com/ramiro/ernesto/nicestart/SplashScreen.java
+++ app/src/main/java/com/ramiro/ernesto/nicestart/SplashScreen.java
@@ -3,8 +3,10 @@ package com.ramiro.ernesto.nicestart;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.annotation.RequiresApi;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
@@ -16,6 +18,7 @@ import android.widget.TextView;
  */
 
 public class SplashScreen extends Activity {
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,9 +28,12 @@ public class SplashScreen extends Activity {
         TextView mySubtitle = (TextView)findViewById(R.id.textView4);
         ImageView myImage = (ImageView)findViewById(R.id.imageView);
 
-        // sets a Pretty Custom Font
-        Typeface myFont = Typeface.createFromAsset(getAssets(), "bromello.otf");
-        myTitle.setTypeface(myFont);
+//        // sets a Pretty Custom Font
+//        Typeface myFont = Typeface.createFromAsset(getAssets(), "bromello.otf");
+//        myTitle.setTypeface(myFont);
+
+        Typeface typeface = getResources().getFont(R.font.bromello);
+        myTitle.setTypeface(typeface);
 
         //implements and starts animation
         Animation myanim = AnimationUtils.loadAnimation(this, R.anim.shake);
