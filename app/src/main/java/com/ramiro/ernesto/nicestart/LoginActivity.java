37de4ea280d071711147e6dc03784bbe4f868e28
Ernesto Ramiro <atomms@gmail.com>
2019-01-15 20:42:15 +0100

    splash centered

--- app/src/main/java/com/ramiro/ernesto/nicestart/LoginActivity.java
+++ app/src/main/java/com/ramiro/ernesto/nicestart/LoginActivity.java
@@ -2,10 +2,12 @@ package com.ramiro.ernesto.nicestart;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.AnimatedVectorDrawable;
 import android.os.Bundle;
 import android.support.design.widget.TextInputLayout;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 //import android.support.v7.app.ActionBarActivity;
@@ -16,6 +18,7 @@ public class LoginActivity extends Activity {
    protected TextView mSignUpTextview;
     protected Button mLoginButton;
     protected TextInputLayout mUsername;
+    protected ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
