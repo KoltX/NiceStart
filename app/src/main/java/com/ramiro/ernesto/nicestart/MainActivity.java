8d6e7cd46e1ffa2280f09bf08f9249f42e8aa0a5
Ernesto Ramiro <atomms@gmail.com>
2019-01-15 17:31:13 +0100

    alpha, author

--- app/src/main/java/com/ramiro/ernesto/nicestart/MainActivity.java
+++ app/src/main/java/com/ramiro/ernesto/nicestart/MainActivity.java
@@ -4,6 +4,10 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * @author ernesto
+ */
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
