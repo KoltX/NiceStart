1a5c5a395ee4b69f03edaf148597319844628f81
Ernesto Ramiro <atomms@gmail.com>
2019-01-21 16:27:05 +0100

    swiperefresh+circular avatar+menus

--- app/src/main/java/com/ramiro/ernesto/nicestart/LoginActivity.java
+++ app/src/main/java/com/ramiro/ernesto/nicestart/LoginActivity.java
@@ -2,7 +2,6 @@ package com.ramiro.ernesto.nicestart;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.drawable.AnimatedVectorDrawable;
 import android.os.Bundle;
 import android.support.design.widget.TextInputLayout;
 import android.view.View;
--- app/src/main/java/com/ramiro/ernesto/nicestart/MainActivity.java
+++ app/src/main/java/com/ramiro/ernesto/nicestart/MainActivity.java
@@ -1,20 +1,124 @@
 package com.ramiro.ernesto.nicestart;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-/**
- * @author ernesto
- * petando...
- */
+import android.support.design.widget.Snackbar;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SwipeRefreshLayout swipeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_context);
-        
+        setContentView(R.layout.activity_main);
+
+//        cast a la vista a la que aplicamos un menu contextual y la registramos
+        TextView mycontext = (TextView) findViewById(R.id.textView);
+        registerForContextMenu(mycontext);
+
+//       cast al Layout SwipeRefresh con el que rodeamos la vista en el xml y le colocamos un listener
+        swipeLayout = (SwipeRefreshLayout) findViewById(R.id.myswipe);
+        swipeLayout.setOnRefreshListener(mOnRefreshListener);
+
     }
+
+
+    //    construimos el Swipe y aplicamos un Listener que lanza un SnackBar y desactiva a continuación del Swipe la animación
+    protected SwipeRefreshLayout.OnRefreshListener mOnRefreshListener = new SwipeRefreshLayout.OnRefreshListener() {
+        @Override
+        public void onRefresh() {
+
+//    opción TOAST
+//
+//          Toast toast0 = Toast.makeText(MainContextActivity.this, "going swipeREFRESH", Toast.LENGTH_LONG);
+//          toast0.show();
+
+
+//   opción SNACKBAR
+
+            final RelativeLayout mrelativeLayout = (RelativeLayout) findViewById(R.id.activity_main_context);
+
+            Snackbar snackbar = Snackbar
+                    .make(mrelativeLayout, "fancy a Snack while you refresh?", Snackbar.LENGTH_LONG)
+                    .setAction("UNDO", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            Snackbar snackbar1 = Snackbar.make(mrelativeLayout, "Action is restored!", Snackbar.LENGTH_SHORT);
+                            snackbar1.show();
+                        }
+                    });
+
+            snackbar.show();
+            swipeLayout.setRefreshing(false);
+
+        }
+    };
+
+    //    inflamos el menu del AppBar con los items del recurso xml
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    //    creamos una lista de eventos para los items del menu del AppBar
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Toast toast = Toast.makeText(this, "going Settings", Toast.LENGTH_LONG);
+            toast.show();
+        } else if (id == R.id.camera) {
+            Toast toast = Toast.makeText(this, "going APPBaR CAMERA", Toast.LENGTH_LONG);
+            toast.show();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    //    inflamos el menu contextual con los items del menu xml correspondiente
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_context, menu);
+
+    }
+
+    //    creamos una lista de eventos para los items del menus contextual
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        switch (item.getItemId()) {
+            case R.id.camera:
+                Toast toast = Toast.makeText(this, "going CONTEXT CAMERA", Toast.LENGTH_LONG);
+                toast.show();
+                break;
+            case R.id.action_settings:
+                Toast toast2 = Toast.makeText(this, "going CONTEXT SETTINGS", Toast.LENGTH_LONG);
+                toast2.show();
+                break;
+        }
+        return super.onContextItemSelected(item);
+    }
+
+
 }
