ee976c5f99b8205ba004c866c87027011f22bd82
Ernesto Ramiro <atomms@gmail.com>
2020-11-05 10:30:07 +0100

    loginville

---
+++ app/src/main/java/com/ramiro/ernesto/nicestart/LoginActivityTry.java
@@ -0,0 +1,13 @@
+package com.ramiro.ernesto.nicestart;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivityTry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_try);
+    }
+}
