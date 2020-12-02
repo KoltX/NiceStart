b87b9236c99d403256ea5ca87bedff664d430c28
Ernesto Ramiro <atomms@gmail.com>
2020-12-02 11:28:05 +0100

    cleared javadoc again

--- app/src/main/java/com/ramiro/ernesto/nicestart/SplashScreen.java
+++ app/src/main/java/com/ramiro/ernesto/nicestart/SplashScreen.java
@@ -16,10 +16,6 @@ import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 
 
-/**
- * @author ernesto
- */
-
 public class SplashScreen extends Activity {
     //    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
